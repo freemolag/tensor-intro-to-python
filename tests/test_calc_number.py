c94260720f87da18721a3a63d40302e327b6946e
freemolag <129sorok@gmail.com>
2021-04-23 09:38:23 +0300

    linter

--- tests/test_calc_number.py
+++ tests/test_calc_number.py
@@ -4,8 +4,10 @@ from tensor_intro_to_python.calc_number import calc_number
 def test_answer_1():
     assert calc_number(0) == 0
 
+
 def test_answer_2():
     assert calc_number(1) == 123
+
 
 def test_answer_3():
     assert calc_number(9) == 1107
