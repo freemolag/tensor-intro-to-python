09972a3e1b1c09229d39061b38b454a926904886
freemolag <129sorok@gmail.com>
2021-05-07 17:44:41 +0300

    add palindrome linter

--- tests/test_palindrom.py
+++ tests/test_palindrom.py
@@ -2,11 +2,12 @@ from tensor_intro_to_python.is_palindrom import is_palindrom
 
 
 def test_answer_1():
-    assert is_palindrom(121) == True
+    assert True == is_palindrom(121) == True  # noqa
 
 
 def test_answer_2():
-    assert is_palindrom(1331) == True
+    assert is_palindrom(1331) == True  # noqa
 
-def test_answer_2():
-    assert is_palindrom(1333) == False
+
+def test_answer_3():
+    assert is_palindrom(1333) == False  # noqa
