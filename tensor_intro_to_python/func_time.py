f2654a300a0d761038bd902fe80b69177b2bab0e
freemolag <129sorok@gmail.com>
2021-11-29 14:47:33 +0300

    lint

--- tensor_intro_to_python/func_time.py
+++ tensor_intro_to_python/func_time.py
@@ -1,19 +1,23 @@
 """
-Напишите декоратор func_time, который подсчитывает  и выводит сколько времени выполняется функция, обернутая в него.
+Напишите декоратор func_time, который подсчитывает
+и выводит сколько времени выполняется функция, обернутая в него.
 """
 
 import time
 from tensor_intro_to_python.amount_lucky import amount_lucky
 
+
 def func_time(fn):
     def wrapper():
         start_time = time.time()
         fn()
-        print("Функция some_func выполнялсь %s сек" % (time.time() - start_time))
+        print("Функция some_func выполнялсь %s сек"
+              % (time.time() - start_time))
     return wrapper
+
 
 @func_time
 def some_func():
     amount_lucky()
 
-# some_func() 
+# some_func()
